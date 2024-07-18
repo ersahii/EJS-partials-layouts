11f33e8dcb22b50bc38c0c071b06f1aba29356b2
mirmehraj <mehraj95he@gmail.com>
2024-07-18 15:13:47 +0530

    Handling routes using express

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 import express from "express";
 const app = express();
 const port = 3000;
-app.use(express.static("/public"));
+app.use(express.static("public"));
 app.get("/", (req , res)=>{
     res.render('home.ejs')
+})
+app.get("/about" , (req, res)=>{
+    res.render("about.ejs")
+})
+app.get("/services" , (req, res)=>{
+    res.render("services.ejs")
+})
+app.get("/contact" , (req, res)=>{
+    res.render("contact.ejs")
 })
 app.listen(port , ()=>{
     console.log(`Server started at port ${port}`);
