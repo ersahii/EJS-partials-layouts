adebcfebe7630d5e138f7c7deade50fe542e5344
Mir Mehraj Mushtaq <mehraj95he@gmail.com>
2024-07-18 09:56:57 +0530

    Created initial files

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+const port = 3000;
+app.use(express.static("/public"));
+app.get("/", (req , res)=>{
+    res.render('home.ejs')
+})
+app.listen(port , ()=>{
+    console.log(`Server started at port ${port}`);
+})
